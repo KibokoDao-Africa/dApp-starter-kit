a84f309eda1cb97c7a7b6b1db6715df1ea37baaf
Steve Kimoi <moenisteve@gmail.com>
2023-05-04 10:25:20 +0300

    modifying hardhat scriptto deploy to mumbai netowork

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config(); 
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  defaultNetwork: "polygon_mumbai",
+  networks: {
+    hardhat: {}, 
+    polygon_mumbai: {
+      url: process.env.ALCHEMY_API_KEY, 
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
