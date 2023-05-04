48e206b243d385a1809bd1f2771d6cef48be1a3a
Steve Kimoi <moenisteve@gmail.com>
2023-05-04 10:11:33 +0300

    function for creating and adding the files containing JSON abi and contract address

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,47 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
+const hre = require("hardhat"); 
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+const main = async () => {
+  // Get deployer address and log it 
+  const [deployer] = await ethers.getSigners(); 
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  console.log("Deploying contract with the account: ", deployer.address);
+  
+  // Get contract factory and deploy the contract
+  const contractFactory = await ethers.getContractFactory("SimpleStorage"); 
+  const simpleStorage = await contractFactory.deploy(); 
+  await simpleStorage.deployed(); 
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  console.log("SimpleStorage contract address: ", simpleStorage.address); 
 
-  await lock.deployed();
+  // Save the contract's artifacts and address in the frontend directory
+  saveFrontendFiles(simpleStorage); 
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
+const saveFrontendFiles = (simpleStorage) => {
+  const fs = require("fs"); 
+  const contractsDir = __dirname + "/../frontend/src/contracts"; 
+
+  if(!fs.existsSync(contractsDir)){
+    fs.mkdirSync(contractsDir)
+  }
+
+  fs.writeFileSync(
+    contractsDir + '/contracts-address.json', 
+    JSON.stringify({ SimpleStorage: simpleStorage.address }, undefined, 2)
+  ); 
+
+  const SimpleStorageArtifact = hre.artifacts.readArtifactSync("SimpleStorage"); 
+
+  fs.writeFileSync(
+    contractsDir + '/SimpleStorage.json', 
+    JSON.stringify(SimpleStorageArtifact, null, 2)
+  ); 
+
+}
+
+main().then(() => process.exit(0)).catch(error => {
   console.error(error);
-  process.exitCode = 1;
+  process.exit(1);
 });
