d18673bd8caa40fc53d7494fc44a51b4d5a9db97
Stephen Kimoi <moenisteve@gmail.com>
2023-05-23 08:49:18 +0300

    removing the alchemy rpc provider

--- frontend/src/WalletFunctionalities/WagmiWallet.js
+++ frontend/src/WalletFunctionalities/WagmiWallet.js
@@ -7,13 +7,13 @@ import { publicProvider } from 'wagmi/providers/public'
 import { CoinbaseWalletConnector } from '@wagmi/core/connectors/coinbaseWallet'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
-import config from '../../config'
+// import config from '../../config'
 
-const alchemyRpcProvider = config.rpcProvider; 
+// const alchemyRpcProvider = config.rpcProvider; 
 
 const { chains, provider , webSocketProvider } = configureChains(
     [polygonMumbai], // Configure whichever chains you would like to use, look at line 2 for the imports
-    [alchemyProvider({ apiKey: alchemyRpcProvider }), publicProvider()], // Change the alchemyRpcProvider to your own provider
+    [alchemyProvider({ apiKey: 'yourAlchemyApiKey' }), publicProvider()], // Change the alchemyRpcProvider to your own provider
 )
 
 export const client = createClient({
